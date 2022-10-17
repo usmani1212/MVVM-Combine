0b3acddb0425edc0d47cc42a364aaeb2268aa4a2
usmani1212 <usman.m@letsumai.com>
2022-10-17 14:41:36 +0500

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Muhammad Usman on 17/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
